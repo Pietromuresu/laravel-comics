e0e557ff8441e35f528bdeda95bfe682ec6744d2
Pietromuresu <muresupietro66@gmail.com>
2023-06-05 21:17:53 +0200

    contact us

--- resources/views/partials/contacts.blade.php
+++ resources/views/partials/contacts.blade.php
@@ -2,4 +2,24 @@
 
 
 @section('content')
+
+<div class="contacts-container">
+    <div class="container">
+
+        <h1>Contact Us Here</h1>
+
+        <div class="form-container">
+
+            <div class="mb-3 w-50">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter here your Email">
+            </div>
+            <div class="mb-3 w-75">
+                <label for="exampleFormControlTextarea1" class="form-label">Type here your message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+
+        </div>
+    </div>
+</div>
 @endsection
